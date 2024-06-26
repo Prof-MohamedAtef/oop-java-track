515436b9e286f46ad02799d82d0b5ef46f945cba
Mohamed Atef <proofesser@gmail.com>
2024-06-26 01:05:24 -0700

    inherit Abstract classes Dog and Cat and calling their methods on Main

---
+++ src/abstraction/Animal.java
@@ -0,0 +1,60 @@
+package abstraction;
+
+import abstraction.interfaces.Animal;
+
+abstract class Animal {
+    // Abstract method (does not have a body)
+    public abstract void makeSound();
+    
+    // Abstract method (does not have a body)
+    public abstract void see();
+
+   
+    // Regular method
+    public void sleep() {
+        System.out.println("This animal is sleeping.");
+    }
+
+    // Regular method
+    public void eat() {
+        System.out.println("This animal is eating.");
+    }
+}
+/*
+ * solution 1 : extend abstract classes
+ * solution 2 : implement interface
+ */
+/*
+  * solution 1 : extend abstract classes
+ */
+ /* 
+ class Dog extends Animal{
+
+    @Override
+    public void makeSound() {
+        System.out.println("The dog says: Woof Woof");
+    }
+
+    @Override
+    public void see() {
+        System.out.println("Watching ...");
+    }
+}
+
+class Cat extends Animal{
+
+    @Override
+    public void makeSound() {
+        System.out.println("The cat says: Meow Meow");
+    }
+
+    @Override
+    public void see() {
+        System.out.println("Watching ...");
+    }
+
+}*/
+
+/*
+ * solution 2 : implement interface
+ */
---
+++ src/abstraction/Main.java
@@ -0,0 +1,19 @@
+package abstraction;
+
+public class Main {
+    public static void main(String[] args) {
+
+        // * solution 1 : extend abstract classes
+        /*Animal dog = new Dog();
+        dog.eat();
+        dog.makeSound();
+        dog.see();
+        dog.sleep();
+        
+        Animal cat = new Cat();
+        cat.eat();;
+        cat.makeSound();
+        cat.see();
+        cat.sleep();*/
+    }
+}
