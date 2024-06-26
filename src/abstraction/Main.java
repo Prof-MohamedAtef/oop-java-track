14b4558ca0dd5f15ee58a986dbab7b3f1b0efc25
Mohamed Atef <proofesser@gmail.com>
2024-06-26 02:31:29 -0700

    added default, private, static method, and constant to the interface

--- src/abstraction/Main.java
+++ src/abstraction/Main.java
@@ -27,6 +27,6 @@ public class Main {
         AnimalListener horseListener = new Horse();
         horseListener.eat();
         horseListener.makeSound();
-        
+               
     }
 }
--- src/abstraction/interfaces/AnimalListener.java
+++ src/abstraction/interfaces/AnimalListener.java
@@ -1,6 +1,40 @@
 package abstraction.interfaces;
 
+import java.util.Locale.Category;
+
+/*
+ * An interface is defined using the interface keyword.
+ * Interfaces can contain abstract methods, 
+ *  default methods,
+ *  static methods,
+ *  and constants.
+ */
+
 public interface AnimalListener {
+    //constant
+    String CATEGORY = "Girraph";
+
+    //Abstract method
     void makeSound();
     void eat();    
+
+    // Default method
+    default void sleep(){
+        System.out.println("This animal is sleeping");
+    }
+
+    // Static method (Java 8+)
+    static void printAnimal(){
+        System.out.println("Animal: " + CATEGORY);
+    }
+
+    // private method (JAva 9+)
+    private void privateMethod(){
+        System.out.println("This is a private method in the interface.");
+    }
+
+    // Default method using private method (Java 9+)
+    default void showPrivate(){
+        privateMethod();
+    }
 }
